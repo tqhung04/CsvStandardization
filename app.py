c7dec6191f46ddb960d0172c9e43e40fb8a1ff01
HungTran <tqhung04@gmail.com>
2019-09-06 09:27:39 +0900

    Add react

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static/dist', template_folder='./static')
 
 @app.route('/')
-def hello():
-  return 'Hello HungTranTGF'
-
+def index():
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)

754baa11391a285736dc33bad20fba91452b1bd3
Hung Tran <tqhung04@gmail.com>
2019-09-07 11:52:06 +0900

    Init Webpack

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CsvStandardization
+
+## Init Project
+### Build Docker image
+`cd CsvStandardization`
+`docker build -t [image_name] .`
+
+### Start a python server
+`docker-compose up`
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,24 +1,21 @@
-const webpack = require('webpack');
-const config = {
-    entry:  __dirname + '/js/index.jsx',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          exclude: /node_modules/,
-          use: 'babel-loader'
+module.exports = {
+  entry:  __dirname + '/js/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    resolveLoader: {
-      modules: ['node_modules'],
-    }
+      }
+    ]
+  }
 };
-module.exports = config;
